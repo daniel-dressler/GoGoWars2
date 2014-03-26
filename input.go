d2622ddc7493ae6e77e32859e94cb9b4e90dde79
Daniel <danieru.dressler@gmail.com>
2014-03-25 23:34:14 -0600

    Add message box framework

--- input.go
+++ input.go
@@ -6,11 +6,12 @@ import (
 )
 
 type Input struct {
-
+	mb *Messagebox
 }
 
 func new_input() Input {
 	var this Input
+	this.mb = new_messagebox()
 	return this
 }
 
@@ -18,6 +19,10 @@ func (this Input) update() {
 	var event sdl.Event
 	for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 		switch t := event.(type) {
+		case *sdl.QuitEvent:
+			msg := new_message(QuitMsg)
+			this.mb.send(msg)
+
 		case *sdl.MouseMotionEvent:
 			fmt.Printf("[%d ms] MouseMotion\tid:%d\tx:%d\ty:%d\txrel:%d\tyrel:%d\n", t.Timestamp, t.Which, t.X, t.Y, t.XRel, t.YRel)
 		}
--- state.go
+++ state.go
@@ -1,15 +1,23 @@
 package main
 
 type State struct {
-
+	mb *Messagebox
 }
 
 func new_state() State {
 	var this State
+	this.mb = new_messagebox()
+	this.mb.listen(QuitMsg)
 	return this
 }
 
 func (this State) plan() bool {
+	for _, msg := range (*this.mb.get()) {
+		switch msg.token {
+		case QuitMsg:
+			return false
+		}
+	}
 	return true
 }
 
